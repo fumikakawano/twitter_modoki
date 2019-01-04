ad74936cc42a08f8ecbdbd77a7a64dd8846f26c0
fumikakawano <fumi_yot@outlook.com>
2019-01-04 07:45:33 +0000

    “[Add tweet create

--- tweet_modoki/app/controllers/tweets_controller.rb
+++ tweet_modoki/app/controllers/tweets_controller.rb
@@ -1,20 +1,19 @@
 class TweetsController < ApplicationController
   def index
-  end
-
-  def show
+    @tweets = Tweet.all
+    @tweet = Tweet.new
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.save
+    redirect_to "/tweets/"
   end
 
-  def update
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
-
+  private
+   def tweet_params
+     params.require(:tweet).permit(
+       :tweet
+    )
+   end
 end
--- tweet_modoki/config/routes.rb
+++ tweet_modoki/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :tweets
+
   get 'likes/index'
-  get 'tweets/index'
   get 'users/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
